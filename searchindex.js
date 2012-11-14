3c3e2c7228546a165c065e8f71bca17897853145
Simon Forman <forman.simon@gmail.com>
2012-11-14 15:02:09 -0800

    Change the title.

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({objects:{},terms:{assembl:0,index:0,search:0,welcom:0,modul:0,pigeon:0,indic:0,content:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to Pigeon Assembler&#8217;s documentation!"],objnames:{},filenames:["index"]})
+Search.setIndex({objects:{},terms:{assembl:0,index:0,search:0,welcom:[],modul:0,pigeon:0,indic:0,content:0,tabl:0,document:[],page:0},objtypes:{},titles:["Pigeon Assembler"],objnames:{},filenames:["index"]})
