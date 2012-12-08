9fcf2d8370d626a3927f859743293c1cfad90137
Simon Forman <forman.simon@gmail.com>
2012-12-07 21:14:07 -0800

    Minor cleanup.

--- assembler/rest2asm.py
+++ assembler/rest2asm.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python
 '''
-Good enough for now. Read a ReST file and extract all the code definitions
-and print them out. Use this to transform avrvm.rest to avrvm.asm.
+This is a simple script to read a reStructuredText file and extract all
+the code definitions (the "literal-block" content) and print them out.
+
+Use this to transform e.g. docs/pigeon_firmware.rst to asm.py like so::
+
+    ./rest2asm.py ../docs/pigeon_firmware.rst > asm.py
+
 '''
+# There's probably a way to get the content directly from the docutils
+# stuff without going through HTML output and BeautifulSoup parsing but I
+# don't have the spare time to figure it out at the moment.  (And this
+# works fine.)
 import sys
 from BeautifulSoup import BeautifulStoneSoup
 from docutils.core import publish_parts
 
+
 if len(sys.argv) > 1:
-    data = open(sys.argv[-1]).read()
+  data = open(sys.argv[-1]).read()
 else:
-    data = sys.stdin.read()
+  data = sys.stdin.read()
+
 
 parts = publish_parts(data, writer_name='html')
+html_body = parts['html_body']
 soup = BeautifulStoneSoup(
-    parts['html_body'],
-    convertEntities=BeautifulStoneSoup.ALL_ENTITIES,
-    )
+  html_body,
+  convertEntities=BeautifulStoneSoup.ALL_ENTITIES,
+  )
+
 
 for p in soup.findAll('pre', { "class" : "literal-block" }):
-    for c in p.contents:
-        sys.stdout.write(c)
+  for c in p.contents:
+    sys.stdout.write(c)
+
