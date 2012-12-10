f394cb0a2d776c1fac0e85e19786b577e061db52
Simon Forman <forman.simon@gmail.com>
2012-12-09 19:42:02 -0800

    Some docs for the main asm object.

--- assembler/pyavrasm.py
+++ assembler/pyavrasm.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+'''
+====================
+Python AVR Assembler
+====================
+
+
+'''
 import pprint
 from collections import defaultdict
 from struct import pack
@@ -16,10 +24,20 @@ from util import (
 
 class DirectivesMixin(object):
   '''
-  I am a docstring!
+  These are *directives*, assembler functions that don't correspond to op
+  codes but instead do some sort of other function.
   '''
 
   def define(self, **defs):
+    '''
+    Update one or more names in the execution namespace.  The main
+    difference between using this function and simply setting a variable
+    in your asm code is that this function automatically converts
+    integer value(s) into ``intbv`` object(s).
+
+    :param defs: ``<name>=<value>`` pairs
+    :rtype: ``None``
+    '''
     for k, v in defs.iteritems():
       if isinstance(v, int):
         defs[k] = v = ibv(v)
@@ -27,11 +45,45 @@ class DirectivesMixin(object):
     self.context.update(defs)
 
   def org(self, address):
+    '''
+    Set the current output address of the assembly process to
+    ``address``.  If ``address`` isn't an ``intbv`` it is converted to
+    one.
+
+    :param address: Location in program.
+    :type address: ``intbv``, ``int``, or symbolic label.
+    :rtype: ``None``
+    '''
     address = ibv(address)
     print 'setting org to %#06x' % (address,)
     update(self.here, address)
 
   def label(self, label_thunk, reserves=0):
+    '''
+    Create a symbolic label at the current output address of the assembly
+    process. If ``reserves`` is given (and greater than zero) that many
+    bytes are reserved by adding the value to the current output address.
+
+    :param label_thunk: An ``intbv`` object serving as a container for a
+       pointer value to an address in your assembly program.
+
+       When you mention a previously unused name in your asm code the
+       execution context will automatically provide that name with a new
+       ``intbv`` object initialized to zero.
+
+       Because this ``intbv`` object will be (re-)used in other parts of
+       your program wherever the name is used it becomes a container for
+       the eventual value (a thunk) of the address.
+
+       When you use this *directive* with a given named address thunk
+       (label) it fills in the value of the current output address of the
+       assembly process.
+    :type label_thunk: ``intbv``
+    :param reserves: Reserve this many bytes by increasing the current
+       output address of the assembly process.
+    :type reserves: ``int``
+    :rtype: ``None``
+    '''
     assert isinstance(label_thunk, intbv), repr(label_thunk)
     assert label_thunk == 0, repr(label_thunk)
     name = self._name_of_address_thunk(label_thunk)
@@ -42,6 +94,13 @@ class DirectivesMixin(object):
       self.here += reserves
 
   def dw(self, *values):
+    '''
+    Lay down unsigned 16-bit integer values in the program image.
+
+    :param values: Integer values to assemble.
+    :type values: iterable of ``int``
+    :rtype: ``None``
+    '''
     addr = self._get_here()
     data = compute_dw(values)
     nbytes = len(data)
@@ -50,6 +109,14 @@ class DirectivesMixin(object):
     self.here += nbytes
 
   def db(self, *values):
+    '''
+    Lay down bytes in the program image. Integers 0 <= n <= 255 and
+    strings are accepted.
+
+    :param values: Values to assemble.
+    :type values: iterable of ``int`` and/or ``string`` values
+    :rtype: ``None``
+    '''
     addr = self._get_here()
     data = compute_db(values)
     nbytes = len(data)
@@ -65,9 +132,47 @@ class DirectivesMixin(object):
 
 
 class AVRAssembly(InstructionsMixin, DirectivesMixin, object):
+  '''
+  This is the primary assembler object.  It is created out of the
+  InstructionsMixin and DirectivesMixin, which together define the
+  functions that you use in your assembly code, and the methods in this
+  class which run the assembly proper.
+
+  Assembling a file is a two-pass process.
+
+  First, the text of the asm
+  code is passed to the ``assemble()`` method (or you can pass a file name
+  to ``assemble_file()``) which builds up an internal model of the op
+  codes to be assembled.
+
+  Second, you call ``pass2()`` which converts the internal model of the
+  op codes to be assembled into a dictionary that maps addresses in the
+  output machine code to the byte strings of the data that should reside
+  at those addresses.
+
+  Then you're probably going to want to call the ``to_hex()`` method to
+  get that binary data out as (the contents of) a hex file, suitable for
+  writing to your ATmega328P.
+  '''
 
   def __init__(self, initial_context=None):
+
+    #: This is the execution context for your assembly file.  It is used
+    #: as the namespace for ``exec`` or ``execfile`` for parsing and
+    #: running your code.
+    #:
+    #: Because it's a ``defaultdict`` and the default factory function
+    #: returns ``intbv`` objects **any** name (identifier) that you use
+    #: in your code that is not previously defined will automatically
+    #: generate a new variable binding.
+    #:
+    #: That is how labels work: you simply use a label and it gets its
+    #: own ``intbv`` object.  Then when you use the ``label()``
+    #: *directive* on that label the ``intbv`` gets updated with the
+    #: actual current output address, and that value will be used to
+    #: assemble the proper bit patterns in ``pass2()``.
     self.context = defaultdict(lambda: int2addr(0))
+
     self.context.update(self._instruction_namespace())
     self.context.update(self._directives())
     self.context.update((f.__name__, f) for f in (
@@ -79,18 +184,49 @@ class AVRAssembly(InstructionsMixin, DirectivesMixin, object):
     if initial_context is not None:
       self.context.update(initial_context)
 
+    #: Current output address of the assembly process.
     self.here = int2addr(0)
+
+    #: Internal intermediate data structure.  This holds the output of
+    #: the methods in the ``InstructionsMixin`` used to create the byte
+    #: strings in ``pass2()``.
     self.data = {}
 
   def assemble(self, text):
+    '''
+    Assemble the string asm source code.
+
+    :param text: Assembly source code.
+    :type text: ``str``
+    :rtype: ``None``
+    '''
     exec text in self.context
     del self.context['__builtins__']
 
   def assemble_file(self, filename):
+    '''
+    Assemble asm source code from a named file.
+
+    :param filename: File name of an assembly source code file.
+    :type filename: ``str``
+    :rtype: ``None``
+    '''
     execfile(filename, self.context)
     del self.context['__builtins__']
 
   def pass2(self):
+    '''
+    Second pass of the assembly process.
+
+    Once the asm source code has been assembled into the intermediate
+    form (by ``assemble()`` or ``assemble_file()``) this method converts
+    it into binary strings.
+
+    :rtype: Mapping of addresses to strings (binary data) this data
+       structure is the end result of the assembly process, just before
+       emitting the strings in e.g. Intel HEX format for burning to a
+       chip.
+    '''
     accumulator = {}
     for addr in sorted(self.data):
       instruction = self.data[addr]
