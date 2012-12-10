be51117c430f61b8fba2de42c1afc6ded833f849
Simon Forman <forman.simon@gmail.com>
2012-12-10 12:18:55 -0800

    Stub of runtime support for AVR Meta-II
    Much of the runtime support for the Meta-II meta-compiler on an AVR chip
    is more-or-less available already in the Pigeon firmware.  Interfacing or
    rewriting it shouldn't be much work.
    
    (Note, this file is incomplete GN* etc aren't in it yet.)

---
+++ metacompiler/runtime.asm.py
@@ -0,0 +1,22 @@
+'''
+Runtime support for AVR Meta-II compiler.
+'''
+
+
+def identifier():
+  pass
+
+def copy_literal(literal):
+  pass
+
+def output_line():
+  pass
+
+def startswith(look_for):
+  pass
+
+def number():
+  pass
+
+def string():
+  pass
