f7dfc04fc0f6700d8c108f52fbf400ee0aa2d694
Simon Forman <forman.simon@gmail.com>
2012-12-20 21:39:23 -0800

    Added GPL notice to source files.
    Didn't add it to metaii.metaii and metaii.asm because I didn't write 'em.

--- pigeon/example_config.py
+++ pigeon/example_config.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright © 2012 Simon Forman
+#
+#    This file is part of Pigeon Computer.
+#
+#    Pigeon Computer is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Pigeon Computer is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Pigeon Computer.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# This is an example of configuration for the Pigeon User Interface.
+# It's just a Python file that gets execfile()'d in the context of the
+# PUI system just before loading and starting the GUI.
+#
 from operator import attrgetter
 import pigeon.xerblin.TextViewer
 
