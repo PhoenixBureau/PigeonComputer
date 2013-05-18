a297ddcbc7b69eb99fec6bfd77300cde5cbe8bce
Simon Forman <forman.simon@gmail.com>
2013-05-18 10:38:28 -0700

    I don't know.

--- pigeon/metacompiler/pycola/ice.py
+++ pigeon/metacompiler/pycola/ice.py
@@ -14,27 +14,22 @@ send(WORD, 'setName', 'word')
 SEQ = send(symbol_vt, 'allocate')
 send(SEQ, 'setName', 'sequence')
 
-def literal(frame, value, symbol=LIT):
-  ast = send(ast_vt, 'allocate')
-  send(ast, 'init', symbol, value)
-  frame.append(ast)
-
-def word(frame, name):
-  literal(frame, name, WORD)
-
 
 CONTEXT = send(object_vt, 'delegated')
 CONTEXT.indent = 0
 
 def emit_lit(ast, context):
+  if not hasattr(context, 'indent'): context.indent = 0
   print ' ' * context.indent, repr(ast.data)
 send(CONTEXT, 'addMethod', 'literal', emit_lit)
 
 def emit_word(ast, context):
+  if not hasattr(context, 'indent'): context.indent = 0
   print ' ' * context.indent, '< %s >' % (ast.data,)
 send(CONTEXT, 'addMethod', 'word', emit_word)
 
 def eval_seq(ast, context):
+  if not hasattr(context, 'indent'): context.indent = 0
   context.indent += 3
   print ' ' * context.indent, '/----\\'
   for item in ast.data:
@@ -48,16 +43,23 @@ if __name__ == '__main__':
     from metaii import comp
     from pprint import pprint
 
-
     machine = open('metaii.asm').read()
     cola_metaii = open('cola.metaii').read()
 
     cola_machine = comp(cola_metaii, machine)
     source = '''
-    2 4 add }
-    baz 'bar' foo q }
-    second ^ first }
-    ^^k } .
+    ! LIT [ symbol_vt <- allocate ] [ LIT <- setName 'literal' ]
+    ! WORD [ symbol_vt <- allocate ] [ WORD <- setName 'word' ]
+    ! SEQ [ symbol_vt <- allocate ] [ SEQ <- setName 'sequence' ]
+
+    ! context [ object_vt <- delegated ]
+    [ context <- addMethod 'literal' emit_lit ]
+    [ context <- addMethod 'word' emit_word ]
+    [ context <- addMethod 'sequence' eval_seq ] 
+
+    ! ast [ ast_vt <- allocate ] [ ast <- init LIT 'Danny' ]
+    [ ast <- eval context ]
+    .
     '''
 
     body = 'def a():\n' + comp(source, cola_machine)
@@ -65,6 +67,15 @@ if __name__ == '__main__':
     exec body
     ast = a()
     pprint(ast)
-    a = ast[0]
-    print; print 'Evaluating', a
-    send(a, 'eval', CONTEXT)
+##    a = ast[0]
+##    print; print 'Evaluating', a
+##    send(a, 'eval', CONTEXT)
+
+
+##
+##source = '''
+##    2 4 add }
+##    baz 'bar' foo q }
+##    second ^ first }
+##    ^^k } .
+##    '''
