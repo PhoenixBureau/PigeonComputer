232f9cbb4a04ff6f29a97213be37a88edc77a3e2
Simon Forman <forman.simon@gmail.com>
2013-10-13 18:33:37 -0700

    Narrow it down.

--- pigeon/metacompiler/pycola/bnforth.py
+++ pigeon/metacompiler/pycola/bnforth.py
@@ -1,18 +1,3 @@
-'''
-
-Each term examines the stack on entry. if false, the word exits immediately with false on the stack.
-
-
-
-|| given a true value on the stack, exits the colon definition immediately with true on the stack.
-
-&& given a false value on the stack, exits the colon definition immediately with false on the stack.
-
-We assume that each "token" (terminal) is represented by a Forth word which scans the input stream and 
-returns a success flag.
-
-'''
-
 class Context(object):
 
   def __init__(self, text, stack=[]):
@@ -21,31 +6,18 @@ class Context(object):
     self.text = text
     self.pointer = 0
 
-  def advance(self):
-    self.pointer += 1
-
-  def parse(self, *terms):
-    C(*terms)(self)
-
-  def current(self):
-    return self.text[self.pointer]
-
-  def push(self):
-    self.stack.append(self.pointer)
-
-  def fail(self):
-    self.pointer = self.stack.pop()
-
-  def okay(self):
-    self.stack.pop()
+  def advance(self): self.pointer += 1
+  def parse(self, *terms): C(*terms)(self)
+  def current(self): return self.text[self.pointer]
+  def push(self): self.stack.append(self.pointer)
+  def fail(self): self.pointer = self.stack.pop()
+  def okay(self): self.stack.pop()
 
   def __repr__(self):
     return '<Context %s %r, %i %r>' % (
       self.success, self.current(), self.pointer, self.stack)
 
-
 class PopFrame(Exception): pass
-
 
 def OR(context):
   if context.success:
@@ -53,7 +25,6 @@ def OR(context):
     raise PopFrame
   context.fail()
   context.success = True
-
 
 def C(*terms):
   def c(context):
@@ -64,7 +35,6 @@ def C(*terms):
     except PopFrame:
       pass
   return c
-
 
 def deco_chk(f):
   def chk(context):
@@ -77,8 +47,6 @@ def deco_chk(f):
         context.okay()
   return chk
 
-
-
 def make_char_tokenizer(char):
   @deco_chk
   def tok(context):
@@ -89,24 +57,12 @@ def make_char_tokenizer(char):
       context.advance()
   return tok
 
+H, h, e, l = map(make_char_tokenizer, 'Hhel')
 
-H = make_char_tokenizer('H')
-h = make_char_tokenizer('h')
-e = make_char_tokenizer('e')
-l = make_char_tokenizer('l')
-
-
-##h = C(h, OR, H, e, l, l)
-
-Context('Hello World').parse(h, OR, H, e, l, l)
-print
-Context('hello World').parse(h, OR, H, e, l, l)
-print
-Context('Hello World').parse(C(h, OR, H), e, l, l)
-print
+Context('Hello World').parse(h, OR, H, e, l, l) ; print
+Context('hello World').parse(h, OR, H, e, l, l) ; print
+Context('Hello World').parse(C(h, OR, H), e, l, l) ; print
 
 c = Context('hello World')
 C(h, OR, H)(c)
 C(e, l, l)(c)
-
-
