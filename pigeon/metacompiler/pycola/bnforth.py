f9a4580867e73840c4727dcd309102bd58fe939f
Simon Forman <forman.simon@gmail.com>
2013-10-13 18:20:52 -0700

    Reworked to use a scan pointer.

--- pigeon/metacompiler/pycola/bnforth.py
+++ pigeon/metacompiler/pycola/bnforth.py
@@ -15,27 +15,32 @@ returns a success flag.
 
 class Context(object):
 
-  def __init__(self, stream, stack=[]):
+  def __init__(self, text, stack=[]):
     self.success = True
     self.stack = stack
-    self.stream = Stream(stream, True)
+    self.text = text
+    self.pointer = 0
+
+  def advance(self):
+    self.pointer += 1
 
   def parse(self, *terms):
     C(*terms)(self)
 
+  def current(self):
+    return self.text[self.pointer]
+
+  def push(self):
+    self.stack.append(self.pointer)
+
+  def fail(self):
+    self.pointer = self.stack.pop()
+
+  def okay(self):
+    self.stack.pop()
+
   def __repr__(self):
-    return '<Context %s %r>' % (self.success, self.stream.current)
-
-
-class Stream(object):
-  def __init__(self, thing, start=False):
-    self.i = iter(thing)
-    self._thing = thing
-    self.current = None if not start else next(self.i)
-
-  def next(self):
-    it = self.current = next(self.i)
-    return it
+    return '<Context %s %r, %i>' % (self.success, self.current(), self.pointer)
 
 
 class PopFrame(Exception): pass
@@ -43,12 +48,15 @@ class PopFrame(Exception): pass
 
 def OR(context):
   if context.success:
+    context.okay()
     raise PopFrame
+  context.fail()
   context.success = True
 
 
 def C(*terms):
   def c(context):
+    context.push()
     try:
       for term in terms:
         term(context)
@@ -58,17 +66,26 @@ def C(*terms):
 
 
 def deco_chk(f):
-  return lambda context: f(context) if context.success else None
+  def chk(context):
+    if context.success:
+      context.push()
+      f(context)
+      if not context.success:
+        context.fail()
+      else:
+        context.okay()
+  return chk
+
 
 
 def make_char_tokenizer(char):
   @deco_chk
   def tok(context):
     print context, '=?=', char
-    if context.stream.current != char:
+    if context.current() != char:
       context.success = False
     else:
-      context.stream.next()
+      context.advance()
   return tok
 
 
@@ -83,7 +100,8 @@ j = C(C(h, OR, H), e, l, l)
 
 Context('Hello World').parse(h)
 print
-Context('hello World').parse(h)
+c = Context('hello World')
+c.parse(h)
 print
 
 Context('Hello World').parse(j)
