ee579e2b8aad8495bc51ce161611166808828f1c
Simon Forman <forman.simon@gmail.com>
2013-10-17 15:42:08 -0700

    Some polish.

--- pigeon/metacompiler/pycola/bubbles.py
+++ pigeon/metacompiler/pycola/bubbles.py
@@ -2,8 +2,16 @@ from co import bootstrap, send, addMethod, lookup
 from la import setUpTransformEngine
 
 
+#########################################################################
+##  COLA Machinery
+
+
 object_vt, vtvt = bootstrap()
 symbol_vt, ast_vt = setUpTransformEngine(object_vt, vtvt)
+
+
+#########################################################################
+##  Abstract Syntax Tree
 
 
 def allocate(vt):
@@ -17,6 +25,9 @@ def make_kind(kind):
   return send(allocate(symbol_vt), 'setName', kind)
 def make_ast(KIND, value):
   return send(allocate(ast_vt), 'init', KIND, value)
+
+def name_of_symbol_of(ast):
+  return send(send(ast, 'typeOf'), 'getName')
 
 
 SYMBOL = make_kind('symbol')
@@ -32,6 +43,10 @@ def literal(value):
 
 def list_(*values):
   return make_ast(LIST, list(values))
+
+
+#########################################################################
+##  Parser
 
 
 def advance(context):
@@ -172,6 +187,10 @@ def finish_frame(context):
   send(context, 'finish_frame')
 
 
+#########################################################################
+##  Little Language
+
+
 from string import whitespace
 blanc = [OR] * (len(whitespace) * 2 - 1)
 blanc[::2] = map(chartok, whitespace) #Python is cool. ;)
@@ -206,22 +225,121 @@ def do_list(context):
 little_language = seq(__, kstar(do_list), dot)
 
 
+#########################################################################
+##  LISP-ish Evaluator
+
+
+def evaluate(ast, context):
+  sname = name_of_symbol_of(ast)
+  if sname == 'symbol':
+    try:
+      return send(context, 'lookup', ast.data)
+    except:
+      return str(ast.data) + '?'
+
+  if sname == 'literal':
+    return ast.data
+
+  first, rest = ast.data[0], ast.data[1:]
+  sname = name_of_symbol_of(first)
+  if sname == 'symbol':
+    if first.data == 'define':
+      define(rest, context)
+      return
+    if first.data == 'lambda':
+      return make_lambda_ast(rest, context)
+
+  exp = tuple(evaluate(it, context) for it in ast.data)
+  if callable(exp[0]):
+    return exp[0](*exp[1:])
+  return exp
+
+
+def define((var, exp), context):
+  send(context, 'addMethod', var.data, evaluate(exp, context))
+
+
+def make_lambda_ast((variables, exp), context):
+  variables = tuple(v.data for v in variables.data)
+  exp = list_(*exp.data)
+  def inner(*args):
+    new_context = send(context, 'delegated')
+    for k, v in zip(variables, args):
+      send(new_context, 'addMethod', k, v)
+    return evaluate(exp, new_context)
+  return inner
+
+
+def evaluate_list(ast, context):
+  result = evaluate(ast, context)
+  if result is not None:
+    print '<', result, '>'
+
+
+#########################################################################
+##  Simple AST Pretty Printer
+
+
+# We can also use machinery like this to walk the AST and print a
+# representation.
+
+def emit_lit(ast, context):
+  print ' ' * context.indent, repr(ast.data)
+
+def emit_word(ast, context):
+  print ' ' * context.indent, '< %s >' % (ast.data,)
+
+def emit_seq(ast, context):
+  context.indent += 3
+  print ' ' * context.indent, '/----\\'
+  for item in ast.data:
+    send(item, 'eval', context)
+  print ' ' * context.indent, '\\____/'
+  context.indent -= 3
+
+
+def prep_emit_context(context):
+  send(context, 'addMethod', 'literal', emit_lit)
+  send(context, 'addMethod', 'symbol', emit_word)
+  send(context, 'addMethod', 'list', emit_seq)
+  context.indent = 0
+
+
+#########################################################################
+##  Put it all together and you haxz a neat little environment.
+
+
 if __name__ == '__main__':
   from pprint import pprint
-  from nonon import print_context, evaluate_list
 
-  c = makeContext(" (12) ( 3 'fo ur' ) ( 5 (6 (7) bo )bo) .")
+  c = makeContext('''
+
+  (12) ( 3 'fo ur' ) ( 5 (6 (7) bo )bo)
+
+  (define p (divide 1 1000000000))
+  (define pi (multiply 3141592653 p))
+
+  (define area (lambda (r) (multiply pi (multiply r r))))
+
+  ( area 23 nic )
+
+  .''')
 
   little_language(c)
 
   del c.data['context'] ; print ; print ; pprint(c.data) ; print
 
+  prep_emit_context(c)
   for it in c.data['current_frame']:
-    send(it, 'eval', print_context)
+    send(it, 'eval', c)
     print
   print
 
+  send(c, 'addMethod', 'multiply', lambda x, y: y * x)
+  send(c, 'addMethod', 'divide', lambda x, y: x / float(y))
+
   send(c, 'addMethod', 'list', evaluate_list)
+
   for it in c.data['current_frame']:
     send(it, 'eval', c)
     print
