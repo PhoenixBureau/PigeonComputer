fa0aa30854a56fc8f776314d13d70e4347cd6bda
Simon Forman <sforman@hushmail.com>
2021-04-22 11:05:19 -0700

    Put newlang5py into its own directory.

---
+++ pigeon/metacompiler/newlang5py/newlang5py.asm
@@ -0,0 +1,284 @@
+    ADR PROGRAM
+PROGRAM
+    TST '▶'
+    BF L1
+    ID
+    BE
+    LB
+    CL 'from metaii_base import MetaII, ParseError'
+    NL
+    NL
+    NL
+    CL 'class Compiler(MetaII):'
+    NL
+    LMI
+    NL
+    CL 'def compile(self, source):'
+    LMI
+    NL
+    CL 'self.input = source'
+    NL
+    CL 'self.reset()'
+    NL
+    CL 'self.rule_'
+    CI
+    CL '()'
+    NL
+    CL 'return self.output.getvalue()'
+    LMD
+    NL
+    NL
+L2
+    CLL ST
+    BT L2
+    SET
+    BE
+    LMD
+    NL
+    CL 'if __name__ == "__main__":'
+    LMI
+    NL
+    CL 'import sys'
+    NL
+    CL 'c = Compiler()'
+    NL
+    CL 'source = sys.stdin.read()'
+    NL
+    CL 'out = c.compile(source)'
+    NL
+    CL 'print(out)'
+    LMD
+    NL
+    TST '◀'
+    BE
+L1
+L3
+    R
+ST
+    ID
+    BF L4
+    CL 'def rule_'
+    CI
+    CL '(self):'
+    LMI
+    NL
+    CL 'rname = "'
+    CI
+    CL '"'
+    NL
+    CL 'rlabel = 0'
+    NL
+    TST '→'
+    BE
+    CLL EX1
+    BE
+    TST '▪'
+    BE
+    LMD
+    NL
+L4
+L5
+    R
+EX1
+    CLL EX2
+    BF L6
+L7
+    TST '|'
+    BF L8
+    CL 'if not self.switch:'
+    LMI
+    NL
+    CLL EX2
+    BE
+    LMD
+    NL
+L8
+L9
+    BT L7
+    SET
+    BE
+L6
+L10
+    R
+EX2
+    CLL EX3
+    BF L11
+    CL 'if self.switch:'
+    LMI
+    NL
+L11
+    BT L12
+    CLL OUTPUT
+    BF L13
+    CL 'if True:'
+    LMI
+    NL
+L13
+L12
+    BF L14
+L15
+    CLL EX3
+    BF L16
+    CL 'if not self.switch:'
+    LMI
+    NL
+    CL 'raise ParseError(rname)'
+    LMD
+    NL
+    NL
+L16
+    BT L17
+    CLL OUTPUT
+    BF L18
+L18
+L17
+    BT L15
+    SET
+    BE
+    CL 'pass'
+    LMD
+    NL
+L14
+L19
+    R
+EX3
+    ID
+    BF L20
+    CL 'self.rule_'
+    CI
+    CL '()'
+    NL
+L20
+    BT L21
+    SR
+    BF L22
+    CL 'self.TST('
+    CI
+    CL ')'
+    NL
+L22
+    BT L21
+    TST '●'
+    BF L23
+    CL 'self.ID()'
+    NL
+L23
+    BT L21
+    TST 'ℕ'
+    BF L24
+    CL 'self.NUM()'
+    NL
+L24
+    BT L21
+    TST '≋'
+    BF L25
+    CL 'self.SR()'
+    NL
+L25
+    BT L21
+    TST '('
+    BF L26
+    CLL EX1
+    BE
+    TST ')'
+    BE
+L26
+    BT L21
+    TST '∅'
+    BF L27
+    CL 'self.switch = True'
+    NL
+L27
+    BT L21
+    TST '★'
+    BF L28
+    CL 'self.switch = True'
+    NL
+    CL 'while self.switch:'
+    LMI
+    NL
+    CLL EX3
+    BE
+    LMD
+    NL
+    CL 'self.switch = True'
+    NL
+L28
+L21
+    R
+OUTPUT
+    TST '«'
+    BF L29
+L30
+    CLL OUT1
+    BT L30
+    SET
+    BE
+    TST '»'
+    BE
+L29
+L31
+    R
+OUT1
+    TST '⊙'
+    BF L32
+    CL 'self.output_buffer += self.last'
+    NL
+L32
+    BT L33
+    SR
+    BF L34
+    CL 'self.output_buffer += '
+    CI
+    NL
+L34
+    BT L33
+    TST '#'
+    BF L35
+    CL 'if rlabel == 0:'
+    LMI
+    NL
+    CL 'rlabel = self.gnlabel'
+    NL
+    CL 'self.gnlabel += 1'
+    LMD
+    NL
+    CL 'self.output_buffer += str(rlabel)'
+    NL
+L35
+    BT L33
+    TST '↵'
+    BF L36
+    CL 'print(self.output_buffer.rstrip(), file=self.output)'
+    NL
+    CL 'self.output_buffer = self._indent * self.indent'
+    NL
+L36
+    BT L33
+    TST '⇤'
+    BF L37
+    CL 'self.output_buffer = ""'
+    NL
+L37
+    BT L33
+    TST '⇥'
+    BF L38
+    CL 'self.output_buffer += self._indent'
+    NL
+L38
+    BT L33
+    TST '↦'
+    BF L39
+    CL 'self.indent += 1'
+    NL
+L39
+    BT L33
+    TST '↤'
+    BF L40
+    CL 'self.indent -= 1'
+    NL
+L40
+L33
+    R
+    END
+
---
+++ pigeon/metacompiler/newlang5py/newlang5py.py
@@ -0,0 +1,404 @@
+from metaii_base import MetaII, ParseError
+
+
+class Compiler(MetaII):
+
+    def compile(self, source):
+        self.input = source
+        self.reset()
+        self.rule_PROGRAM()
+        return self.output.getvalue()
+
+    def rule_PROGRAM(self):
+        rname = "PROGRAM"
+        rlabel = 0
+        self.TST('▶')
+        if self.switch:
+            self.ID()
+            if not self.switch:
+                raise ParseError(rname)
+
+            self.output_buffer = ""
+            self.output_buffer += 'from metaii_base import MetaII, ParseError'
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'class Compiler(MetaII):'
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.indent += 1
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'def compile(self, source):'
+            self.indent += 1
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'self.input = source'
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'self.reset()'
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'self.rule_'
+            self.output_buffer += self.last
+            self.output_buffer += '()'
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'return self.output.getvalue()'
+            self.indent -= 1
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.switch = True
+            while self.switch:
+                self.rule_ST()
+
+            self.switch = True
+            if not self.switch:
+                raise ParseError(rname)
+
+            self.indent -= 1
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'if __name__ == "__main__":'
+            self.indent += 1
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'import sys'
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'c = Compiler()'
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'source = sys.stdin.read()'
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'out = c.compile(source)'
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'print(out)'
+            self.indent -= 1
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.TST('◀')
+            if not self.switch:
+                raise ParseError(rname)
+
+            pass
+
+    def rule_ST(self):
+        rname = "ST"
+        rlabel = 0
+        self.ID()
+        if self.switch:
+            self.output_buffer += 'def rule_'
+            self.output_buffer += self.last
+            self.output_buffer += '(self):'
+            self.indent += 1
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'rname = "'
+            self.output_buffer += self.last
+            self.output_buffer += '"'
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.output_buffer += 'rlabel = 0'
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            self.TST('→')
+            if not self.switch:
+                raise ParseError(rname)
+
+            self.rule_EX1()
+            if not self.switch:
+                raise ParseError(rname)
+
+            self.TST('▪')
+            if not self.switch:
+                raise ParseError(rname)
+
+            self.indent -= 1
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            pass
+
+    def rule_EX1(self):
+        rname = "EX1"
+        rlabel = 0
+        self.rule_EX2()
+        if self.switch:
+            self.switch = True
+            while self.switch:
+                self.TST('|')
+                if self.switch:
+                    self.output_buffer += 'if not self.switch:'
+                    self.indent += 1
+                    print(self.output_buffer.rstrip(), file=self.output)
+                    self.output_buffer = self._indent * self.indent
+                    self.rule_EX2()
+                    if not self.switch:
+                        raise ParseError(rname)
+
+                    self.indent -= 1
+                    print(self.output_buffer.rstrip(), file=self.output)
+                    self.output_buffer = self._indent * self.indent
+                    pass
+
+            self.switch = True
+            if not self.switch:
+                raise ParseError(rname)
+
+            pass
+
+    def rule_EX2(self):
+        rname = "EX2"
+        rlabel = 0
+        self.rule_EX3()
+        if self.switch:
+            self.output_buffer += 'if self.switch:'
+            self.indent += 1
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            pass
+        if not self.switch:
+            self.rule_OUTPUT()
+            if self.switch:
+                self.output_buffer += 'if True:'
+                self.indent += 1
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+        if self.switch:
+            self.switch = True
+            while self.switch:
+                self.rule_EX3()
+                if self.switch:
+                    self.output_buffer += 'if not self.switch:'
+                    self.indent += 1
+                    print(self.output_buffer.rstrip(), file=self.output)
+                    self.output_buffer = self._indent * self.indent
+                    self.output_buffer += 'raise ParseError(rname)'
+                    self.indent -= 1
+                    print(self.output_buffer.rstrip(), file=self.output)
+                    self.output_buffer = self._indent * self.indent
+                    print(self.output_buffer.rstrip(), file=self.output)
+                    self.output_buffer = self._indent * self.indent
+                    pass
+                if not self.switch:
+                    self.rule_OUTPUT()
+                    if self.switch:
+                        pass
+
+
+            self.switch = True
+            if not self.switch:
+                raise ParseError(rname)
+
+            self.output_buffer += 'pass'
+            self.indent -= 1
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            pass
+
+    def rule_EX3(self):
+        rname = "EX3"
+        rlabel = 0
+        self.ID()
+        if self.switch:
+            self.output_buffer += 'self.rule_'
+            self.output_buffer += self.last
+            self.output_buffer += '()'
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            pass
+        if not self.switch:
+            self.SR()
+            if self.switch:
+                self.output_buffer += 'self.TST('
+                self.output_buffer += self.last
+                self.output_buffer += ')'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+        if not self.switch:
+            self.TST('●')
+            if self.switch:
+                self.output_buffer += 'self.ID()'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+        if not self.switch:
+            self.TST('ℕ')
+            if self.switch:
+                self.output_buffer += 'self.NUM()'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+        if not self.switch:
+            self.TST('≋')
+            if self.switch:
+                self.output_buffer += 'self.SR()'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+        if not self.switch:
+            self.TST('(')
+            if self.switch:
+                self.rule_EX1()
+                if not self.switch:
+                    raise ParseError(rname)
+
+                self.TST(')')
+                if not self.switch:
+                    raise ParseError(rname)
+
+                pass
+
+        if not self.switch:
+            self.TST('∅')
+            if self.switch:
+                self.output_buffer += 'self.switch = True'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+        if not self.switch:
+            self.TST('★')
+            if self.switch:
+                self.output_buffer += 'self.switch = True'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                self.output_buffer += 'while self.switch:'
+                self.indent += 1
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                self.rule_EX3()
+                if not self.switch:
+                    raise ParseError(rname)
+
+                self.indent -= 1
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                self.output_buffer += 'self.switch = True'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+
+    def rule_OUTPUT(self):
+        rname = "OUTPUT"
+        rlabel = 0
+        self.TST('«')
+        if self.switch:
+            self.switch = True
+            while self.switch:
+                self.rule_OUT1()
+
+            self.switch = True
+            if not self.switch:
+                raise ParseError(rname)
+
+            self.TST('»')
+            if not self.switch:
+                raise ParseError(rname)
+
+            pass
+
+    def rule_OUT1(self):
+        rname = "OUT1"
+        rlabel = 0
+        self.TST('⊙')
+        if self.switch:
+            self.output_buffer += 'self.output_buffer += self.last'
+            print(self.output_buffer.rstrip(), file=self.output)
+            self.output_buffer = self._indent * self.indent
+            pass
+        if not self.switch:
+            self.SR()
+            if self.switch:
+                self.output_buffer += 'self.output_buffer += '
+                self.output_buffer += self.last
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+        if not self.switch:
+            self.TST('#')
+            if self.switch:
+                self.output_buffer += 'if rlabel == 0:'
+                self.indent += 1
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                self.output_buffer += 'rlabel = self.gnlabel'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                self.output_buffer += 'self.gnlabel += 1'
+                self.indent -= 1
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                self.output_buffer += 'self.output_buffer += str(rlabel)'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+        if not self.switch:
+            self.TST('↵')
+            if self.switch:
+                self.output_buffer += 'print(self.output_buffer.rstrip(), file=self.output)'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                self.output_buffer += 'self.output_buffer = self._indent * self.indent'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+        if not self.switch:
+            self.TST('⇤')
+            if self.switch:
+                self.output_buffer += 'self.output_buffer = ""'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+        if not self.switch:
+            self.TST('⇥')
+            if self.switch:
+                self.output_buffer += 'self.output_buffer += self._indent'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+        if not self.switch:
+            self.TST('↦')
+            if self.switch:
+                self.output_buffer += 'self.indent += 1'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+        if not self.switch:
+            self.TST('↤')
+            if self.switch:
+                self.output_buffer += 'self.indent -= 1'
+                print(self.output_buffer.rstrip(), file=self.output)
+                self.output_buffer = self._indent * self.indent
+                pass
+
+
+
+if __name__ == "__main__":
+    import sys
+    c = Compiler()
+    source = sys.stdin.read()
+    out = c.compile(source)
+    print(out)
+
